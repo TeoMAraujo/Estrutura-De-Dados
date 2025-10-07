1dbf388492fee0c40b5a8133e14ce14affbe1e60
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-10-07 01:47:54 -0300

    syntax

--- TP1/include/vector.hpp
+++ TP1/include/vector.hpp
@@ -1,14 +1,17 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-#include <iostream> // for debugging
+#include <iostream>
+#include <stdexcept>
 
-template <class T> class vector {
+template <class T>
+class vector {
 private:
     T *ptr;
     int size;
+
 public:
-    // Constructor and Destructor
+    // Cinstructor and destructor
     vector();
     vector(int size);
     ~vector();
@@ -29,4 +32,102 @@ public:
     T& operator[](int index);
 };
 
-#endif
+// implementation
+
+template <class T>
+vector<T>::vector() {
+    this->ptr = nullptr;
+    this->size = 0;
+}
+
+template <class T>
+vector<T>::vector(int size) {
+    this->ptr = new T[size];
+    this->size = size;
+}
+
+template <class T>
+vector<T>::~vector() {
+    delete[] this->ptr;
+}
+
+template <class T>
+int vector<T>::get_size() const {
+    return this->size;
+}
+
+template <class T>
+void vector<T>::insert(int pos, T obj) {
+    if (pos < 0 || pos > this->size) {
+        throw std::out_of_range("invalid position");
+    }
+    T* temp = this->ptr;
+    this->ptr = new T[this->size + 1];
+    for (int i = 0; i < pos; ++i) {
+        this->ptr[i] = temp[i];
+    }
+    this->ptr[pos] = obj;
+    for (int i = pos; i < this->size; ++i) {
+        this->ptr[i + 1] = temp[i];
+    }
+    ++this->size;
+    delete[] temp;
+}
+
+template <class T>
+void vector<T>::remove(int pos) {
+    if (pos < 0 || pos >= this->size) {
+        throw std::out_of_range("invalid position");
+    }
+    T* temp = this->ptr;
+    this->ptr = new T[this->size - 1];
+    for (int i = 0; i < pos; ++i) {
+        this->ptr[i] = temp[i];
+    }
+    for (int i = pos + 1; i < this->size; ++i) {
+        this->ptr[i - 1] = temp[i];
+    }
+    --this->size;
+    delete[] temp;
+}
+
+template <class T>
+void vector<T>::push_back(T obj) {
+    this->insert(this->size, obj);
+}
+
+template <class T>
+void vector<T>::push_front(T obj) {
+    this->insert(0, obj);
+}
+
+template <class T>
+void vector<T>::pop_back() {
+    if (this->size > 0) {
+        this->remove(this->size - 1);
+    }
+}
+
+template <class T>
+void vector<T>::pop_front() {
+    if (this->size > 0) {
+        this->remove(0);
+    }
+}
+
+template <class T>
+void vector<T>::clear() {
+    delete[] this->ptr;
+    this->ptr = nullptr;
+    this->size = 0;
+}
+
+template <class T>
+T& vector<T>::operator[](int index) {
+    if (index < 0 || index >= this->size) {
+        throw std::out_of_range("invalid index");
+    }
+    return this->ptr[index];
+}
+
+#endif 
